719ce40fc72df03fff383ca8191bc99602711bb1
shobomaru <shobomaru@gmail.com>
2015-04-06 01:23:41 +0900

    Fix compiler warnings.

--- TextureOptimize/TextureOptimize.cpp
+++ TextureOptimize/TextureOptimize.cpp
@@ -239,9 +239,8 @@ public:
 				nullptr,
 				IID_PPV_ARGS(mTex.ReleaseAndGetAddressOf())));
 			mTex->SetName(L"Texure");
-			D3D12_BOX box = {};
 			int copySrcOffset = 0;
-			for (int i = 0; i < mipSize.size(); ++i)
+			for (int i = 0u; i < mipSize.size(); ++i)
 			{
 				D3D12_BOX box = {};
 				box.right = get<0>(mipSize[i]);
